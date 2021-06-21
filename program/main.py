5cee0ab6e3170daa47302d2786f54625db7f4812
cristianoltfs <cristianoltfs@gmial.com>
2021-06-21 09:20:35 -0300

    imagem títuo janela e fundo

--- program/main.py
+++ program/main.py
@@ -2,5 +2,36 @@
 import pygame
 pygame.init()
 
-if __name__ == "__main__":
-    print("Tudo certo")
+# create the screen
+screen = pygame.display.set_mode((800, 600))
+
+# Background
+background = pygame.image.load('resources/images/tabuleiro.png')
+
+
+# Title and Icon
+pygame.display.set_caption("Perfil da Ergonomia")
+icon = pygame.image.load('resources/images/tabuleiro.png')
+pygame.display.set_icon(icon)
+
+# Game Loop
+running = True
+while running:
+    
+    # RGB
+    screen.fill((255, 255, 255))
+    
+    # Background Image
+    screen.blit(background,(0, 0))
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+#running = True
+
+#if __name__ == "__main__":
+    
+    
+#    print("Tudo certo")
