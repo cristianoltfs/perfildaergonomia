9ce133a1279688c58e0f9c5833e806c62737ac05
cristianoltfs <cristianoltfs@gmial.com>
2021-06-21 23:57:15 -0300

    inserção dos comandos com funções

--- program/main.py
+++ program/main.py
@@ -2,48 +2,44 @@
 import pygame
 pygame.init()
 
+# Title
+pygame.display.set_caption("Perfil da Ergonomia")
+
+# Icon
+icon = pygame.image.load('resources/images/tabuleiro.png')
+pygame.display.set_icon(icon)
 
 # Background
 background = pygame.image.load('resources/images/tabuleiro.png')
 
-
-# Title and Icon
-pygame.display.set_caption("Perfil da Ergonomia")
-icon = pygame.image.load('resources/images/tabuleiro.png')
-pygame.display.set_icon(icon)
-
 #Obtendo tamanho da imagem
-altura_icon= icon.get_height()
-largura_icon = icon.get_width()
+height_background= icon.get_height()
+width_background = icon.get_width()
 
 #Create the screen
-screen = pygame.display.set_mode((largura_icon, altura_icon))
+screen = pygame.display.set_mode((width_background, height_background))
 
-# Game Loop
-running = True
-while running:
-    
-    # RGB
-    screen.fill((255, 255, 255))
-    
-    # Background Image
-    screen.blit(background,(0, 0))
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        #Se esc for pressionado finaliza o programa
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                running = False
+WHITE =(255, 255, 255)
 
-    #Inserindo fundo (imagem de fundo)
-    # screen.blit(icon, (0,0))
+FPS = 30
 
+def draw_window():
+    screen.fill(WHITE)
+    screen.blit(background,(0,0))
     pygame.display.update()
 
-pygame.quit()
-
+def main():
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        draw_window()
+        
+    pygame.quit()
+    
 #running = True
-#if __name__ == "__main__":
-#    print("Tudo certo")
+if __name__ == "__main__":
+    main()
