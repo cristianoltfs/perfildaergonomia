00317fe98e7c86ed7ed043d80bf4ae46818b3894
Lucas Barbosa <barbosa.lucas390@yahoo.com>
2021-06-28 22:44:35 -0300

    Criada pastas do servidor

---
+++ programClient/client.py
@@ -0,0 +1,10 @@
+import socket
+
+cliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+while True:
+    mensagem_envio = input('Digite a mensagem: ')
+
+    cliente.sendto(mensagem_envio.encode(), ('200.239.167.212', 12000))
+    mensagem_bytes, endereco_ip_servidor = cliente.recvfrom(2048)
+    print(mensagem_bytes.decode())
