43c0243747d088dd9c9e2f67e39c2a2f1525b133
Washington Franca <wsenafranca@gmail.com>
2021-06-30 18:46:20 -0300

    um simples chat cliente e servidor utilizando threads

--- programClient/client.py
+++ programClient/client.py
@@ -1,13 +1,68 @@
 import socket
+import threading
+import traceback
 
-HOSTPORT = ('200.239.167.212', 12000)
+#HOSTPORT = ('200.239.167.212', 12000)
+HOSTPORT = ('localhost', 12000)
 
-cliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# encode/decode mensanges para o servidor
+def messageDecode(data):
+       decoded = data.decode()
+       t = int(decoded[0]) # primeiro byte e o tipo
+       b = decoded[1:] # resto e o corpo
+       
+       return t, b
+       
+def messageEncode(messageType, messageBody):
+    return bytes(str(messageType) + messageBody, 'utf-8')
 
-nome = 'Cristiano'
-cliente.sendto(nome.encode(), HOSTPORT)
+# classe que represneta o servidor
+class Server (threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.socket = None
+        
+    def connectServer(self, ip, port):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((ip, port))
+        
+    def run(self):
+        self.isRunning = True
+        while self.isRunning:
+            data = self.socket.recv(2048) # esperando por ate 2048 bytes...
+            messageType, messageBody = messageDecode(data)
+            
+            # a decodificacao e feita por meio do 'messageType'
+            if messageType == 1: # mensagem
+                print(messageBody)
+        
+    def send(self, data):
+        self.socket.sendall(data)
+        
+    def closeServer(self):
+        self.isRunning = False
+
+server = Server()
+
+server.connectServer(HOSTPORT[0], HOSTPORT[1])
+server.start()
+
+username = input('Digite seu nome: ')
+
+server.send(messageEncode(1, username))
+
+print('Entrando...')
 
 while True:
+    try:
+        msg = input('Digite a mensagem: ')
+        if msg == 'sair':
+            server.send(messageEncode(3, msg))
+        else:
+            server.send(messageEncode(2, msg))
+            
+    except Exception as e:
+        print(traceback.format_exc())
     
-    msg = input('Digite a mensagem: ')
-    cliente.sendto(msg.encode(), HOSTPORT)
+server.closeServer()
+server.join()
