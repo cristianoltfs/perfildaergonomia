7468c6d5c120e188140f903db90f7ce86f4c2243
cristianoltfs <cristianoltfs@gmial.com>
2021-07-12 23:04:03 -0300

    enviando mensagem para todos sem travar

--- programClient/ctest5.py
+++ programClient/ctest5.py
@@ -1,7 +1,22 @@
 import socket
+import threading
 
 HOST = '200.239.165.217'
 PORT = 13333
+
+class Server (threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.socket = None
+    def run(self):
+        self.isRunning = True
+        while self.isRunning:
+            if self.socket != None:
+                servmsn = self.socket.recv(2048) # esperando por ate 2048 bytes...
+                messageType, messageBody = servmsn.decode()
+
+
+server = Server()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
@@ -14,8 +29,12 @@ print('Digite a sua sala:')
 sala = input()
 s.sendall(str.encode(sala))
 
+server.start()
+
+
 while True:
 
+    
     servmsn =  s.recv(1024)
     print("Resposta do servidor:" ,servmsn.decode())
 
@@ -28,4 +47,4 @@ while True:
     s.sendall(str.encode(nome))
 
 
-s.close
+s.close()
